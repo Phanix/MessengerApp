7b7db0666cab695e886d251802cc75062ed7e4ce
unknown <fh2game86@gmail.com>
2020-05-20 22:30:21 -0300

    first commit

---
+++ app/src/main/java/com/example/messengerapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.messengerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onSendMessage(View View){
+
+    }
+}
