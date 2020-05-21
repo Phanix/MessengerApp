d9d2cf56baa6a6533e41a98738fc3eb9c4ac67ea
fabiodasilva <fh2game86@gmail.com>
2020-05-21 00:10:34 -0300

    Send message for another activity

--- app/src/main/java/com/example/messengerapp/ReceiveMessageActivity.java
+++ app/src/main/java/com/example/messengerapp/ReceiveMessageActivity.java
@@ -3,6 +3,7 @@ package com.example.messengerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReceiveMessageActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class ReceiveMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
+        String message = getIntent().getStringExtra("message");
+        TextView textViewMessage = findViewById(R.id.message);
+        textViewMessage.setText(message);
     }
 }
